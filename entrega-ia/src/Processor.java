d72d83dd405363f84d1b386de9592bdaf5973c7f
MIGUEL <alu0101031538@cc1425.etsii.ull.es>
2019-05-09 13:38:48 +0100

    Arregladas conversiones numericas

--- entrega-ia/src/Processor.java
+++ entrega-ia/src/Processor.java
@@ -17,9 +17,11 @@ public class Processor {
 	FileWriter aprendizajeT = null;
 	FileWriter aprendizajeNT = null;
 	String filename;
-	HashMap<String, Integer> hashmaptroll = new HashMap<String, Integer>();
-	HashMap<String, Integer> hashmapnotroll = new HashMap<String, Integer>();
-	HashMap<String, Integer> hashmaptotal = new HashMap<String, Integer>();
+	HashMap<String, Double> hashmaptroll = new HashMap<String, Double>();
+	HashMap<String, Double> hashmapnotroll = new HashMap<String, Double>();
+	HashMap<String, Double> hashmaptotal = new HashMap<String, Double>();
+	long acc_troll=0;
+	long acc_notroll=0;
 
 	public static void main(String[] args) {
 		Processor algo = new Processor("entrega-ia/src/CTR_TRAIN.txt");
@@ -43,6 +45,8 @@ public class Processor {
 			aprendizajeT = new FileWriter("aprendizajeT.txt");
 			aprendizajeNT = new FileWriter("aprendizajeNT.txt");
 			String text = null;
+			long frases_troll = 0;
+			long frases_notroll = 0;
 
 			while ((text = reader.readLine()) != null) {
 				if (text.isEmpty()) {
@@ -56,21 +60,23 @@ public class Processor {
 				// escribe en el fichero troll o notroll
 				if (valores[1].trim().equalsIgnoreCase("troll")) {
 					// System.out.println(valores[1] + " : troll");
+					frases_troll++;
 					troll.write(valores[0] + "\n");
 					// contar las palabras
 					for (String linea : valores[0].split("\\s+")) {
 						if (!hashmaptroll.containsKey(linea)) {
-							hashmaptroll.put(linea, 1);
+							hashmaptroll.put(linea, 1d);
 						} else {
 							hashmaptroll.put(linea, hashmaptroll.get(linea) + 1);
 						}
 					}
 				} else {
 					// System.out.println(valores[1] + " : not_troll");
+					frases_notroll++;
 					notroll.write(valores[0] + "\n");
 					for (String linea : valores[0].split("\\s+")) {
 						if (!hashmapnotroll.containsKey(linea)) {
-							hashmapnotroll.put(linea, 1);
+							hashmapnotroll.put(linea, 1d);
 						} else {
 							hashmapnotroll.put(linea, hashmapnotroll.get(linea) + 1);
 						}
@@ -79,22 +85,27 @@ public class Processor {
 				corpus.write(valores[0] + "\n");
 			}
 
-			// merge hash maps
-			hashmaptotal.putAll((Map<? extends String, ? extends Integer>) hashmaptroll.clone());
+			// merge hash maps and get the acumulate tokens
+			hashmaptotal.putAll((Map<? extends String, ? extends Double>) hashmaptroll.clone());
 			hashmapnotroll.forEach((key, value) -> {
 				if (hashmaptotal.containsKey(key)) {
 					hashmaptotal.put(key, hashmaptotal.get(key) + value);
+					acc_notroll += value;
 				} else {
 					hashmaptotal.put(key, value);
+					acc_notroll += value;
 				}
 			});
 
+			hashmaptroll.forEach((key, value) -> {
+				acc_troll += value;				
+			});
 			// iterate through total hashmap onto dictionary
-			Iterator<?> it = ((HashMap<String, Integer>) hashmaptotal.clone()).entrySet().iterator();
+			Iterator<?> it = ((HashMap<String, Double>) hashmaptotal.clone()).entrySet().iterator();
 			dict.append("Numero de palabras total:" + hashmaptotal.size() + "\n");
 
 			while (it.hasNext()) {
-				Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+				Map.Entry<String, Double> pair = (Map.Entry<String, Double>) it.next();
 				dict.write("Numero de palabras:" + pair.getValue() + "\n");
 				dict.write("Palabra:" + pair.getKey() + "\n");
 				it.remove(); // avoids a ConcurrentModificationException
@@ -102,25 +113,26 @@ public class Processor {
 
 			// iterate through troll hashmap onto troll learn model
 			it = hashmaptroll.entrySet().iterator();
-			aprendizajeT.append("Numero de documentos del corpus :<número entero>\n" + "Número de palabras del corpus:"
-					+ hashmaptroll.size() + "\n");
+			aprendizajeT.append("Numero de documentos del corpus :"+frases_troll+"\n" + "Nï¿½mero de palabras del corpus:"
+					+ acc_troll + "\n");
 
 			while (it.hasNext()) {
-				Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+				Map.Entry<String, Double> pair = (Map.Entry<String, Double>) it.next();
 				aprendizajeT.write("Palabra:" + pair.getKey() + " Frec:" + pair.getValue() + " LogProb:"
-						+ Math.log(pair.getValue() / hashmaptotal.get(pair.getKey())) + "\n");
+						+ Math.log((pair.getValue()+1) / (acc_troll+hashmaptotal.size()+1)) + "\n");
 				it.remove(); // avoids a ConcurrentModificationException
 			}
 
 			// iterate through no troll hashmap onto no troll learn model
 			it = hashmapnotroll.entrySet().iterator();
-			aprendizajeNT.append("Numero de documentos del corpus :<número entero>\n" + "Número de palabras del corpus:"
-					+ hashmapnotroll.size() + "\n");
+			aprendizajeNT.append("Numero de documentos del corpus :"+frases_notroll+"\n" + "Nï¿½mero de palabras del corpus:"
+					+ acc_notroll + "\n");
 
 			while (it.hasNext()) {
-				Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+				Map.Entry<String, Double> pair = (Map.Entry<String, Double>) it.next();
 				aprendizajeNT.write("Palabra:" + pair.getKey() + " Frec:" + pair.getValue() + " LogProb:"
-						+ Math.log(pair.getValue() / hashmaptotal.get(pair.getKey())) + "\n");
+						+ Math.log((pair.getValue()+1) / (acc_notroll+hashmaptotal.size()+1)) + "\n");
+				System.out.println((pair.getValue()+1) / (acc_notroll+hashmaptotal.size()+1)+" = "+(pair.getValue()+1)+"/"+acc_notroll+"+"+hashmaptotal.size()+1);
 				it.remove(); // avoids a ConcurrentModificationException
 			}
 		} catch (FileNotFoundException e) {
